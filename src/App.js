2973ca5e48f5bbb97ec93ecb894baf2b1cf5a5c1
alinaalbrecht <alinaalbrecht@mailbox.org>
2021-07-26 20:00:10 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>U.S. Presidents Memory Game</h1>
+    <p>Don't pick the same president twice!</p>
+
+    </div>
+  );
+}
+
+export default App;
